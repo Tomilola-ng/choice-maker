83fd1fb9750bf1743c86123d952e29e9bc2de7f5
Tomilola Oluwafemi <93817917+Tomilola-ng@users.noreply.github.com>
2024-12-25 19:00:30 +0100

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/reusable/Header";
+// import Header from "@/components/reusable/Header";
 import DecisionMaker from "./DecisionMaker"
 
 export default function HomePage() {
